be98f5df4f262c66c218260a6eeb1965b1485201
Von'Erik Barren-Tucker <barrentucker.vonerik@gmail.com>
2020-09-12 14:14:50 -0400

    testing form submissions both with and without Form element

--- FrontEnd/Javascript/index.js
+++ FrontEnd/Javascript/index.js
@@ -7,3 +7,17 @@ function addData() {
   document.getElementById("rows").appendChild(newRow);
   document.getElementById("personName").value = '';
 }
+
+function addWorkoutData() {
+  var newWorkout = document.getElementById("workout").value;
+  var newWeight = document.getElementById("weight").value;
+  var newReps = documents.getElementById("reps").value;
+  var newRow = document.createElement("tr");
+  var newCell = document.createElement("td");
+  newCell.innerHTML = workout;
+  newCell.innerHTML = weight;
+  newCell.innerHTML = reps;
+  newRow.append(newCell);
+  document.getElementById("rows").appendChild(newRow);
+  document.getElementById("personName").value = "";
+}
